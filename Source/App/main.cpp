f901e5d4516526d91d590ceded79d6e7b71bbb5a
TheDxr <872151901@qq.com>
2023-05-20 23:21:19 +0800

    fix dependencies error

--- Source/App/main.cpp
+++ Source/App/main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, const char *argv[])
 {
     LogManager::GetInstance().Init();
-    Dxr::Application app{800, 600, 0, 0, "Hello Vulkan!"};
-    app.Run();
-    // DxrApplication::GetInstance().Run();
+//    Dxr::Application app{800, 600, 0, 0, "Hello Vulkan!"};
+//    app.Run();
+    DxrApplication::GetInstance().Run();
     return 0;
 }
